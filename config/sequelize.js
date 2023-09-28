85c3f9228cbaf0de5235c5c453692ba2832b368d
Ramadhan Rahmat <hi.mramadhanrh@gmail.com>
2023-09-28 16:40:21 +0700

    Update sequelize.js

--- config/sequelize.js
+++ config/sequelize.js
@@ -8,6 +8,7 @@ const sequelize = new Sequelize(
   process.env.DB_USERNAME,
   process.env.DB_PASSWORD,
   {
+    port: process.env.DB_PORT,
     host: process.env.DB_HOST,
     dialect: process.env.DB_DIALECT,
   }
